3981ece29395cd4fa54fc8eae38f3ebc57314111
2535ShekharMourya <113845874+2535ShekharMourya@users.noreply.github.com>
2025-06-06 00:43:24 +0530

    first commit "hi Bhai"

--- app/src/main/java/com/tarunguptaraja/expensia/ui/auth/view/SplashActivity.kt
+++ app/src/main/java/com/tarunguptaraja/expensia/ui/auth/view/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.tarunguptaraja.expensia.ui.auth.view
 
 import android.os.Bundle
+import android.widget.Toast
 import com.tarunguptaraja.expensia.base.BaseActivity
 import com.tarunguptaraja.expensia.databinding.ActivitySplashBinding
 
@@ -12,5 +13,6 @@ class SplashActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater, null, false)
         setContentView(binding.root)
+        Toast.makeText(this,"HI Tarun bhai",Toast.LENGTH_SHORT).show()
     }
 }
