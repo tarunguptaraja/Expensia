5983b42b54936299010871afa5db918a20a06145
Tarun Gupta <tarun.g@redbasil.in>
2025-06-23 22:51:40 +0530

    splash

--- app/src/main/java/com/tarunguptaraja/expensia/ui/auth/view/SplashActivity.kt
+++ app/src/main/java/com/tarunguptaraja/expensia/ui/auth/view/SplashActivity.kt
@@ -2,17 +2,27 @@ package com.tarunguptaraja.expensia.ui.auth.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.tarunguptaraja.expensia.base.BaseActivity
 import com.tarunguptaraja.expensia.databinding.ActivitySplashBinding
 
 class SplashActivity : BaseActivity() {
 
     private lateinit var binding: ActivitySplashBinding
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater, null, false)
         setContentView(binding.root)
-        startActivity(Intent(this, AuthenticationActivity::class.java))
+        handler.postDelayed({
+            startActivity(Intent(this, AuthenticationActivity::class.java))
+        }, 1500)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
     }
 }
