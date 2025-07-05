10c4b89392d143cda374616e70921810fb03405f
Tarun Gupta <tarun.g@redbasil.in>
2025-07-05 19:59:11 +0530

    changes

--- app/src/main/java/com/tarunguptaraja/expensia/ui/auth/adapter/OnboardingViewpagerAdapter.kt
+++ app/src/main/java/com/tarunguptaraja/expensia/ui/auth/adapter/OnboardingViewpagerAdapter.kt
@@ -1,4 +1,4 @@
-package com.tarunguptaraja.expensia.ui.auth.view
+package com.tarunguptaraja.expensia.ui.auth.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
@@ -9,15 +9,20 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.tarunguptaraja.expensia.R
 
-class OnboardingViewpagerAdapter(private val imageList: List<Int>, val context: Context, private val titlelist:List<String>, private val discriptionList:List<String>):RecyclerView.Adapter<OnboardingViewpagerAdapter.ViewPagerViewHolder>() {
-    class ViewPagerViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+class OnboardingViewpagerAdapter(
+    private val imageList: List<Int>,
+    val context: Context,
+    private val titlelist: List<String>,
+    private val discriptionList: List<String>
+) : RecyclerView.Adapter<OnboardingViewpagerAdapter.ViewPagerViewHolder>() {
+    class ViewPagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView = itemView.findViewById<ImageView>(R.id.titleImage)
-        val title = itemView.findViewById<TextView>(R.id.texttitle)
-        val description = itemView.findViewById<TextView>(R.id.textdeccription)
+        val title = itemView.findViewById<TextView>(R.id.tv_title)
+        val description = itemView.findViewById<TextView>(R.id.tv_description)
 
-        fun bindData(image:Int,text1:String,text2: String){
+        fun bindData(image: Int, text1: String, text2: String) {
             imageView.setImageResource(image)
-            title.text= text1
+            title.text = text1
             description.text = text2
 
         }
@@ -25,15 +30,16 @@ class OnboardingViewpagerAdapter(private val imageList: List<Int>, val context: 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
 
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.slider_layout, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.slider_layout, parent, false)
         return ViewPagerViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
-      val item = imageList[position]
+        val item = imageList[position]
         val item2 = titlelist[position]
         val item3 = discriptionList[position]
-        holder.bindData(item,item2,item3)
+        holder.bindData(item, item2, item3)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/tarunguptaraja/expensia/ui/auth/view/OnboardingActivity.kt
+++ app/src/main/java/com/tarunguptaraja/expensia/ui/auth/view/OnboardingActivity.kt
@@ -1,40 +1,24 @@
 package com.tarunguptaraja.expensia.ui.auth.view
 
 import android.os.Bundle
-import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.tarunguptaraja.expensia.R
+import com.tarunguptaraja.expensia.base.BaseActivity
+import com.tarunguptaraja.expensia.databinding.ActivityOnboardingBinding
+import com.tarunguptaraja.expensia.ui.auth.adapter.OnboardingViewpagerAdapter
 
-class OnboardingActivity : AppCompatActivity() {
-    lateinit var iv1: ImageView
-    lateinit var iv2: ImageView
-    lateinit var iv3: ImageView
-    lateinit var onboardingViewpager: ViewPager2
+class OnboardingActivity : BaseActivity() {
     lateinit var onboardingViewpagerAdapter: OnboardingViewpagerAdapter
+    private lateinit var binding: ActivityOnboardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_onboarding)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        iv1 = findViewById(R.id.iv1)
-        iv2 = findViewById(R.id.iv2)
-        iv3 = findViewById(R.id.iv3)
-        onboardingViewpager = findViewById(R.id.slideViewPager)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupViewPagerAdapter()
-        onboardingViewpager.registerOnPageChangeCallback(object :
+        binding.slideViewPager.registerOnPageChangeCallback(object :
             ViewPager2.OnPageChangeCallback() {
             override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
+                position: Int, positionOffset: Float, positionOffsetPixels: Int
             ) {
                 changeColor()
                 super.onPageScrolled(position, positionOffset, positionOffsetPixels)
@@ -48,52 +32,50 @@ class OnboardingActivity : AppCompatActivity() {
             override fun onPageScrollStateChanged(state: Int) {
                 changeColor()
                 super.onPageScrollStateChanged(state)
-
             }
-
         })
-
-
     }
 
     fun setupViewPagerAdapter() {
-        val imageList =
-            listOf(R.drawable.onboarding_illust1, R.drawable.onboarding_illust2, R.drawable.onboarding_illust3)
-        val titleList = listOf("Gain total control \n" + "of your money","Know where your \n " + "money goes","Planning ahead")
-        val discriptionList = listOf( "Become your own money manager \n" +
-                " and make every cent count","Track your transaction easily, \n" +
-                "  with categories and financial report", "Setup your budget for each category \n" +
-                "  so you in control")
-        onboardingViewpagerAdapter = OnboardingViewpagerAdapter(imageList, this,titleList,discriptionList)
-        onboardingViewpager.adapter = onboardingViewpagerAdapter
+        val imageList = listOf(
+            R.drawable.onboarding_illust1,
+            R.drawable.onboarding_illust2,
+            R.drawable.onboarding_illust3
+        )
+        val titleList = listOf(
+            "Gain total control \n" + "of your money",
+            "Know where your \n " + "money goes",
+            "Planning ahead"
+        )
+        val discriptionList = listOf(
+            "Become your own money manager \n" + " and make every cent count",
+            "Track your transaction easily, \n" + "  with categories and financial report",
+            "Setup your budget for each category \n" + "  so you in control"
+        )
+        onboardingViewpagerAdapter =
+            OnboardingViewpagerAdapter(imageList, this, titleList, discriptionList)
+        binding.slideViewPager.adapter = onboardingViewpagerAdapter
     }
 
     fun changeColor() {
-        when (onboardingViewpager.currentItem) {
+        when (binding.slideViewPager.currentItem) {
             0 -> {
-                iv1.setBackgroundColor(applicationContext.resources.getColor(R.color.violet_100))
-                iv2.setBackgroundColor(applicationContext.resources.getColor(R.color.violet_40))
-                iv3.setBackgroundColor(applicationContext.resources.getColor(R.color.violet_40))
-
+                binding.iv1.setBackgroundColor(resources.getColor(R.color.violet_100, null))
+                binding.iv2.setBackgroundColor(resources.getColor(R.color.violet_40, null))
+                binding.iv3.setBackgroundColor(resources.getColor(R.color.violet_40, null))
             }
 
-            1 -> { iv1.setBackgroundColor(applicationContext.resources.getColor(R.color.violet_40))
-                iv2.setBackgroundColor(applicationContext.resources.getColor(R.color.violet_100))
-                iv3.setBackgroundColor(applicationContext.resources.getColor(R.color.violet_40))
-
-
+            1 -> {
+                binding.iv1.setBackgroundColor(resources.getColor(R.color.violet_40, null))
+                binding.iv2.setBackgroundColor(resources.getColor(R.color.violet_100, null))
+                binding.iv3.setBackgroundColor(resources.getColor(R.color.violet_40, null))
             }
 
-
-            2 -> { iv1.setBackgroundColor(applicationContext.resources.getColor(R.color.violet_40))
-                iv2.setBackgroundColor(applicationContext.resources.getColor(R.color.violet_40))
-                iv3.setBackgroundColor(applicationContext.resources.getColor(R.color.violet_100))
-
-
+            2 -> {
+                binding.iv1.setBackgroundColor(resources.getColor(R.color.violet_40, null))
+                binding.iv2.setBackgroundColor(resources.getColor(R.color.violet_40, null))
+                binding.iv3.setBackgroundColor(resources.getColor(R.color.violet_100, null))
             }
         }
-
     }
-
-
 }
